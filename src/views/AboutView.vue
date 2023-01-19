07d27e668a75d9256daea32f5606ffde8ad6bbdd
tomylee78 <tomylee78@gmai.com>
2023-01-19 15:46:33 +0800

    第一波

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.axios.get("https://randomuser.me/api/").then((response) => {
+      console.log(response.data);
+    });
+  },
+};
+</script>
