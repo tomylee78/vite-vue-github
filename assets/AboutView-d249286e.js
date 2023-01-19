4c966e215bfb077489768966523e22040a975e80
tomylee78 <tomylee78@gmai.com>
2023-01-19 18:55:02 +0800

    deploy

---
+++ assets/AboutView-d249286e.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,a}from"./index-78201d62.js";const n={data(){return{}},mounted(){this.axios.get("https://randomuser.me/api/").then(e=>{console.log(e.data)})}},_={class:"about"},c=a("h1",null,"This is an about page",-1),r=[c];function i(e,d,u,p,l,h){return o(),s("div",_,r)}const f=t(n,[["render",i]]);export{f as default};
